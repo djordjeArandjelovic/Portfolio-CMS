b9cbb778c38a38944648c2842005aa628efb7a62
Djordje Arandjelovic <60182523+djordjeArandjelovic@users.noreply.github.com>
2024-11-04 22:06:43 +0100

    Update data.js
    Update jobs

--- src/data.js
+++ src/data.js
@@ -1,6 +1,22 @@
 export const jobsData = [
 	{
 		id: 1,
+		role: "iOS Developer",
+		years: "Mar 2024-",
+		company: "SrediMe",
+		description:
+			"Maintaining and developing new features in a Swift UIKit app.",
+	},
+	{
+		id: 2,
+		role: "iOS Developer",
+		years: "Jan 2024-",
+		company: "Skuda MIll",
+		description:
+			"Developing a new SwiftUI app and integrating iOS app with web software.",
+	},
+	{
+		id: 3,
 		role: "Frontend Developer",
 		years: "2023-",
 		company: "Inbox ITS",
@@ -8,7 +24,7 @@ export const jobsData = [
 			"Working as a part of a frontend team, using HTML, CSS, JavaScript, JQuery, JSP, XML...",
 	},
 	{
-		id: 1,
+		id: 4,
 		role: "Product Manager",
 		years: "2021-2023",
 		company: "Skuda Mill",
@@ -16,7 +32,7 @@ export const jobsData = [
 			"Working as a product manager, making sure all customer needs are met through the whole software life cycle, colaborating with HR to hire new developers and working closly with our marketing team.",
 	},
 	{
-		id: 2,
+		id: 5,
 		role: "Factoring Assistant Manager",
 		years: "2020-2023",
 		company: "Stelle Corporation",
@@ -24,7 +40,7 @@ export const jobsData = [
 			"Responsible for the management and implementation of factoring service administration in order to meet business objectives.",
 	},
 	{
-		id: 3,
+		id: 6,
 		role: "Logistics Coordinator",
 		years: "2019",
 		company: "Stelle Corporation",
